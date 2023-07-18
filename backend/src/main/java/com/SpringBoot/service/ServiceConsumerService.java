ad2b85d317a4b4a773966686a0d66e712a32c1e0
Varun-z <varungupta924@gmail.com>
2023-07-18 10:11:51 +0530

    selection logic

--- backend/src/main/java/com/SpringBoot/service/ServiceConsumerService.java
+++ backend/src/main/java/com/SpringBoot/service/ServiceConsumerService.java
@@ -2,7 +2,13 @@ package com.SpringBoot.service;
 
 import com.SpringBoot.entities.ServiceConsumer;
 
+import java.util.UUID;
+
 public interface ServiceConsumerService {
     ServiceConsumer register(ServiceConsumer serviceConsumer) throws Exception;
     ServiceConsumer login(ServiceConsumer serviceConsumer) throws Exception;
+
+    void selectPrefferedService(UUID scId, UUID id);
+
+    ServiceConsumer getServiceConsumer(UUID id);
 }
